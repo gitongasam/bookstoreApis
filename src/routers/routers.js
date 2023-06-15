47df602fe3ccaf364d60ce5b065988d91df843de
Christophertechn <njengachris42@gmail.com>
2023-06-15 16:28:02 +0300

    u can be able to add a book and get all books

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
-const {  } = require('../controllers/controllers');
+const { getAllBooks, getBookById, addBook} = require('../controllers/controllers');
 
 const router = express.Router();
+
+router.get('/books', getAllBooks);
+router.get('/books/:id', getBookById);
+router.post('/books',addBook)
 
 module.exports = router;
