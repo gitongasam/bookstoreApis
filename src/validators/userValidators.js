f87e6d9be9d98ad5954c365b7c015684c0c16c72
Joan Kabura <kaburajoan25@gmail.com>
2023-06-19 16:42:07 -0700

    put validation functionality in validators folder

---
+++ src/validators/userValidators.js
@@ -0,0 +1,14 @@
+const Joi = require('joi');
+
+const userSchema = Joi.object({
+    MemberID: Joi.number().required(),
+    Name: Joi.string().required(),
+    Address: Joi.string().required(),
+    ContactNumber: Joi.string().required(),
+    Password: Joi.string().pattern(new RegExp("^[A-Za-z0-9]")).required(),
+    c_password: Joi.ref("Password")
+  
+  }).with("Password", "c_password");
+
+
+  module.exports = userSchema;
