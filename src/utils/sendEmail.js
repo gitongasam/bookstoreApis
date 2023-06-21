3e8be0f461820c7930c08e55d2a77436eb1b1f70
Joan Kabura <kaburajoan25@gmail.com>
2023-06-20 17:14:06 -0700

    added email functionalities to the code

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,22 @@
+const {createTransport} = require("nodemailer");
+require('dotenv').config();
+const email_config = require('../config/emailConfig.js');
+
+const transporter = createTransport(email_config);
+
+async function sendMail(user_email, subject, text){
+    const message_options = {
+        from: process.env.EMAIL_USER,
+        to: user_email,
+        subject: subject,
+        text: text
+    }
+    try {
+        let results =  await transporter.sendMail(message_options)
+        console.log(results);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = sendMail;
